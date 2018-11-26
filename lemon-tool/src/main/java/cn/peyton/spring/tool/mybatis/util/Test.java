d4c97c693c752102c5c70d6dc63f0790467012e4
peyton2919 <fz2919@tom.com>
2018-11-26 16:17:14 +0800

    update tool

---
+++ lemon-tool/src/main/java/cn/peyton/spring/tool/mybatis/util/Test.java
@@ -0,0 +1,22 @@
+package cn.peyton.spring.tool.mybatis.util;
+
+import java.util.UUID;
+
+/**
+ * <h3></h3>
+ * <pre>
+ * @email: <a href="mailto:fz2919@tom.com">fz2919@tom.com</a>
+ * @project name: lemon
+ * @class name: cn.peyton.spring.tool.mybatis.util.Test.java
+ * @create date: 2018/11/26 15:31
+ * @author: <a href="http://www.peyton.cn">peyton</a>
+ * @version: 1.0.0
+ * </pre>
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID());
+    }
+}
