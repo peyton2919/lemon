83993f876c5bf5d789321a2dc550ee3c6c08614e
peyton2919 <fz2919@tom.com>
2018-11-29 16:49:26 +0800

    update web

---
+++ lemon-web/src/main/java/cn/peyton/spring/lemon/controller/mall/CollectController.java
@@ -0,0 +1,22 @@
+package cn.peyton.spring.lemon.controller.mall;
+
+import cn.peyton.spring.mall.service.CollectService;
+import org.springframework.stereotype.Controller;
+import javax.annotation.Resource;
+
+/**
+ * <h3>收藏[商品]  Controller 类 .</h3>
+ * <pre>
+ * @author <a href="http://www.peyton.cn">peyton</a>
+ * @email <a href="mailto:fz2919@tom.com">fz2919@tom.com</a>
+ * @createDate 2018/11/29 16:34:23
+ * @version 1.0.0
+ * </pre>
+*/
+@Controller
+public class CollectController {
+
+	@Resource
+	private CollectService collectService;
+
+}
